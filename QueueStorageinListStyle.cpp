0ffa2bcab0c35d657f7373219f9bda1e4d632ae7
Jounson <52708455+Jounson@users.noreply.github.com>
2019-07-16 10:52:45 +0800

    Add files via upload

---
+++ QueueStorageinListStyle.cpp
@@ -0,0 +1,26 @@
+﻿#include "pch.h"
+#include <iostream>
+#include<stdio.h>
+#include"myqueue.h"
+using namespace std;
+
+int main()
+{
+	Queue q;
+	initQueue(&q);
+
+	for (char ch = 'a'; ch <= 'z'; ch++)
+	{
+		enQueue(&q, ch);
+	}
+
+	resetQueue(&q);
+	while (!isQueueEmpty(&q))
+		printf("%3c", deQueue(&q));
+	clearQueue(&q);
+
+	cout << endl;
+	return 0;
+}
+
+
